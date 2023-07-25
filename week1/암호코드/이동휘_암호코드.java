e96588ef87f137a6dc84bf8ccf21abce4dd9d363
master <laksj113@gmail.com>
2023-07-25 22:10:39 +0900

    이동휘: 암호코드(23.07.26)

---
+++ week1/암호코드/이동휘_암호코드.java
@@ -0,0 +1,11 @@
+package week1.암호코드;
+
+import java.io.*;
+
+public class 이동휘_암호코드 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    }
+}
