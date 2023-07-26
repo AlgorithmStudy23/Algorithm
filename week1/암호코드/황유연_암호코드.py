43c088497d5f06c36e7e9df5176e472bd419c138
YEONI <89332391+yoooyeon@users.noreply.github.com>
2023-07-26 10:57:09 +0900

    황유연:암호코드(23.07.26)

--- week1/암호코드/황유연_암호코드.py
+++ week1/암호코드/황유연_암호코드.py
@@ -1,1 +1,30 @@
-# 예시 파일
+import sys
+
+def dfs(idx, len_, li):
+    global ans, n
+    if idx == len_:
+        tmp = ''
+        for al in li:
+            tmp += chr(int(al) + 96)
+        ans.add(tmp)
+        return
+    if idx > len_:
+        return
+    li.append(n[idx])
+    dfs(idx + 1, len_, li)
+    li.pop()
+    if idx + 2 <= len_ and 1 <= int(n[idx:idx + 2]) <= 26:
+        li.append(n[idx:idx + 2])
+        dfs(idx + 2, len_, li)
+
+
+ans = set()
+n = input()
+for x in n:
+    x = int(x)
+    if x < 1 or x > 26:
+        print(0)
+        sys.exit()
+len_ = len(n)
+dfs(0, len_, [])
+print(len(ans) % 1000000)
