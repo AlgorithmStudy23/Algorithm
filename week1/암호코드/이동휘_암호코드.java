7d8952695fd863a08cc4520cfe0be922e899f16d
master <laksj113@gmail.com>
2023-07-26 11:00:09 +0900

    이동휘: 암호코드(23.07.26)

--- week1/암호코드/이동휘_암호코드.java
+++ week1/암호코드/이동휘_암호코드.java
@@ -3,9 +3,34 @@ package week1.암호코드;
 import java.io.*;
 
 public class 이동휘_암호코드 {
-    public static void main(String[] args) {
+    static final int MOD = 1000000;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String num = br.readLine();
+        int len = num.length();
+        int[] dp = new int[len+1];
+        dp[0] = 1;
+        dp[1] = 1;
 
+        if (num.charAt(0) == '0') {
+            System.out.println(0);
+            return;
+        }
+
+        for(int i = 2; i <= len; i++) {
+            String cur = num.substring(i-2, i);
+            if ((cur.charAt(0) > '2' && cur.charAt(1) == '0') || Integer.parseInt(cur) == 0) {
+                dp[len] = 0;
+                break;
+            }
+            if (Integer.parseInt(cur) <= 26) {
+                dp[i] += dp[i-2];
+            }
+            if (cur.charAt(1) != '0') {
+                dp[i] += dp[i-1];
+            }
+            dp[i] %= MOD;
+        }
+        System.out.println(dp[len]);
     }
 }
