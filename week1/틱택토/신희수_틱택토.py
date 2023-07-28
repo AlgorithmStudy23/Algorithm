c45f4d9d497a8705bdd95028fdbb4726205c6f76
Heeesuu <gmltn0508@icloud.com>
2023-07-28 12:39:42 +0900

    신희수:틱택토(2023.07.28)

--- week1/틱택토/신희수_틱택토.py
+++ week1/틱택토/신희수_틱택토.py
@@ -1,5 +1,3 @@
-
-
 def win(xolist, XO):
     if xolist[0][0] == xolist[1][1] == xolist[2][2]== XO:
         return True
@@ -7,22 +5,20 @@ def win(xolist, XO):
         return True
     if xolist[0][0] == xolist[0][1] == xolist[0][2]== XO:
         return True
-    
+
     if xolist[1][0] == xolist[1][1] == xolist[1][2]== XO:
         return True
     if xolist[2][0] == xolist[2][1] == xolist[2][2]== XO:
         return True
     if xolist[2][0] == xolist[1][1] == xolist[0][2]== XO:
         return True
-    
+
     if xolist[0][1] == xolist[1][1] == xolist[2][1]== XO:
         return True
     if xolist[0][2] == xolist[1][2] == xolist[2][2]== XO:
         return True
-    
+
     return False
-
-
 
 while True:
     xo = input().strip()
@@ -33,7 +29,7 @@ while True:
         ocount = 0
         start = 0
         xolist = [[0] * 3 for _ in range(3)] #3*3 배열
-        
+
         for i in range(3):
             for j in range(3):
                 xolist[i][j] = xo[start]
@@ -42,23 +38,21 @@ while True:
                 if xo[start] == "O":
                     ocount += 1
                 start += 1
-            
-            if xcount > ocount+1 or ocount > xcount:
-                print("invalid")
-                continue
 
-            if ocount + 1 == xcount: #X가 이겼을 때
-                if win(xolist, "X") and not win(xolist, "O"):
-                    print("valid")
-                    continue
-            if ocount == xcount: #O가 이겼을때
-                if win(xolist, "O") and not win(xolist, "X"):
-                    print("valid")
-                    continue
-            if xcount + ocount == 9 and not win(xolist, "O"): # 무승부 상태
+        if xcount > ocount+1 or ocount > xcount:
+            print("invalid")
+            continue
+
+        if ocount + 1 == xcount: #X가 이겼을 때
+            if win(xolist, "X") and not win(xolist, "O"):
                 print("valid")
                 continue
+        if ocount == xcount: #O가 이겼을때
+            if win(xolist, "O") and not win(xolist, "X"):
+                print("valid")
+                continue
+        if xcount + ocount == 9 and not win(xolist, "O"): # 무승부 상태
+            print("valid")
+            continue
 
-            print("invalid")
-
-    
+        print("invalid")
