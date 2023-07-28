313249ad66c8b846dac17deba2d45de7b9ec45df
master <laksj113@gmail.com>
2023-07-28 14:12:15 +0900

    이동휘: 틱택토(23.07.28)

--- week1/틱택토/이동휘_틱택토.java
+++ week1/틱택토/이동휘_틱택토.java
@@ -3,94 +3,80 @@ package week1.틱택토;
 import java.io.*;
 
 public class 이동휘_틱택토 {
-    static int end;
-    static boolean flag;
-    static char[] target;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         while (true) {
-            target = br.readLine().toCharArray();
-            if (target[0] == 'e') {
+            String S = br.readLine();
+
+            if (S.charAt(0) == 'e') {
                 bw.flush();
                 bw.close();
                 return;
             }
-            end = 9;
+
+            char[][] target = new char[3][3];
+            target[0] = S.substring(0, 3).toCharArray();
+            for (int i = 3; i <= 9; i += 3) {
+                target[i/3 - 1] = S.substring(i-3, i).toCharArray();
+            }
+
             int xCnt = 0;
             int oCnt = 0;
-            for (char c : target) {
-                if (c == '.') {
-                    end--;
-                } else if (c == 'X') {
-                    xCnt++;
-                } else {
-                    oCnt++;
+            for (char[] s : target) {
+                for (char c : s) {
+                    if (c == 'X') {
+                        xCnt++;
+                    } else if (c == 'O'){
+                        oCnt++;
+                    }
                 }
             }
-            if (xCnt >= oCnt+2 || oCnt > xCnt) {
+
+            if (xCnt > oCnt+1 || oCnt > xCnt) {
                 bw.write("invalid");
                 bw.newLine();
                 continue;
             }
 
-            flag = false;
-            if (end != 9 && !check(target)) {
-                bw.write("invalid");
-                bw.newLine();
-                continue;
-            }
+            int sum = oCnt + xCnt;
+            boolean xCheck = check(target, 'X');
+            boolean oCheck = check(target, 'O');
+            boolean flag = false;
 
-            dfs('X', 0, new char[9]);
-
-            if (flag) {
-                bw.write("valid");
+            if (sum == 9 && xCnt == oCnt + 1) {
+                flag = !oCheck;
             } else {
-                bw.write("invalid");
+
+                if (!oCheck && xCheck) {
+                    flag = (xCnt == oCnt + 1);
+                } else if (oCheck && !xCheck){
+                    flag = (xCnt == oCnt);
+                }
             }
+
+            bw.write((flag) ? "valid" : "invalid");
             bw.newLine();
         }
     }
-    static void dfs(char cur, int num, char[] arr) {
-        if (num == end) {
-            flag = true;
-            return;
-        }
+    static boolean check(char[][] arr, char cur) {
+        for (int i = 0; i < 3; i++) {
 
-        for (int i = 0; i < 9; i++) {
-            if (target[i] == cur && arr[i] != cur) {
-                arr[i] = cur;
-                num++;
-                if (num != end && check(arr)) {
-                    arr[i] = '.';
-                    return;
-                }
-                dfs((cur == 'X') ? 'O' : 'X', num, arr);
-                num--;
-                arr[i] = '.';
-            }
-        }
-    }
-    static boolean check(char[] arr) {
-        if ((arr[0] == 'X' || arr[0] == 'O')) {
-            if ((arr[0] == arr[1] && arr[1] == arr[2]) || (arr[0] == arr[4] && arr[4] == arr[8])
-            || (arr[0] == arr[3] && arr[3] == arr[6])) {
+            if (arr[i][0] == cur && arr[i][0] == arr[i][1] && arr[i][1] == arr[i][2]) {
                 return true;
             }
-        }
-        if ((arr[4] == 'X' || arr[4] == 'O')) {
-            if ((arr[3] == arr[4] && arr[4] == arr[5]) || (arr[2] == arr[4] && arr[4] == arr[6])
-            || (arr[1] == arr[4] && arr[4] == arr[7])) {
 
+            if (arr[0][i] == cur && arr[0][i] == arr[1][i] && arr[1][i] == arr[2][i]) {
                 return true;
             }
         }
 
-        if ((arr[6] == 'X' || arr[6] == 'O')) {
-            if ((arr[6] == arr[7] && arr[7] == arr[8])
-            || (arr[2] == arr[5] && arr[5] == arr[8])) {
-                return true;
-            }
+        if (arr[0][0] == cur && arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2]) {
+            return true;
+        }
+
+        if (arr[2][0] == cur && arr[2][0] == arr[1][1] && arr[1][1] == arr[0][2]) {
+            return true;
         }
 
         return false;
