518e4b1c0bcdbb901675ca023dbc36bd8f02ea9a
YEONI <89332391+yoooyeon@users.noreply.github.com>
2023-07-31 01:47:02 +0900

    황유연: 암호코드 풀이 수정 (23.07.31)

--- week1/암호코드/황유연_암호코드.py
+++ week1/암호코드/황유연_암호코드.py
@@ -1,1 +1,18 @@
-# 예시 파일
+import sys
+
+MOD = 1000000
+st = input()
+if st[0] == '0':
+    print(0)
+    sys.exit()
+dp = [0 for _ in range(len(st) + 1)]
+dp[0] = dp[1] = 1
+for i in range(1, len(st)):
+    k = i + 1
+    x = st[i]
+    if '10' <= st[i - 1] + x <= '26':
+        dp[k] += dp[k - 2]
+    if '1' <= x <= '9':
+        dp[k] += dp[k - 1]
+    dp[k] = dp[k] % MOD
+print(dp[-1] % MOD)
