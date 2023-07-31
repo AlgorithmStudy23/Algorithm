4c2e455ce6e0d2bf322932b6fb66dc9b9d432bb8
YEONI <89332391+yoooyeon@users.noreply.github.com>
2023-07-31 19:29:18 +0900

    황유연: 징검다리 건너기

--- week2/징검다리_건너기/황유연_징검다리_건너기.py
+++ week2/징검다리_건너기/황유연_징검다리_건너기.py
@@ -1,1 +1,21 @@
 
+def solution(stones, k):
+    left = 1 
+    right = 200000000
+    while left <= right:
+        mid = (left + right) // 2 # 건넌 친구의 수 
+        cnt = 0
+        for x in stones:
+            if x - mid <= 0:
+                cnt += 1
+            else:
+                cnt = 0
+            if cnt >= k:
+                break
+        if cnt >= k: # k 이상 건너 뛰어야 하면 정답 줄이기
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return left
+
