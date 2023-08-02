8a924603cc341c66d513dc2b709d52c37ee31e50
YEONI <89332391+yoooyeon@users.noreply.github.com>
2023-08-02 14:34:13 +0900

    황유연: 후보키 풀이 수정(23.08.02)

--- week2/후보키/황유연_후보기.py
+++ week2/후보키/황유연_후보기.py
@@ -3,34 +3,45 @@ def check_key(res, relation, keys):
     for row in relation:
         tmp_row = []
         for i in range(len(res)):
-            # if row[res[i]] not in tmp:
             tmp_row.append(row[res[i]])
-
         if tmp_row not in tmp:
             tmp.append(tmp_row)
-    # print(tmp)
-    if len(tmp) == len(relation) and res not in keys and is_minimal(res, keys.copy()):
-        print(res)
-        keys.append(res)
+    if len(tmp) == len(relation):
+        if res not in keys:
+            if is_minimal(res, keys.copy()):
+                # print(res)
+                keys.append(res)
+
+# 파이썬 내장 함수를 사용하는 방법
+# def is_minimal(res, keys):
+#     for key in keys:
+#         if set(key).issubset(res):
+#             return False
+#     return True
 
 
 def is_minimal(res, keys):
-    cnt = 0
+
     if len(keys) == 0:
         return True
+
+    # 키를 하나씩 가져와 res의 요소와 몇개가 겹치나 확인한다. 
+    # 겹치는 갯수가 키의 길이 이상이면 최소가 아니라고 판단한다.
     for k in keys:
+        cnt = 0
         for p in res:
             if cnt >= len(k):
                 return False
-                break
             if p in k:
                 cnt += 1
+        if cnt >= len(k):
+            return False
     return True
 
 
 def dfs(idx, n, res, relation, keys):
     if len(res) == n:
-        check_key(res.copy(), relation, keys)
+        check_key(res.copy(), relation, keys) # 주의
         return
     for i in range(idx, len(relation[0])):
         if i not in res:
@@ -44,3 +55,4 @@ def solution(relation):
     for i in range(1, len(relation[0]) + 1):
         dfs(0, i, [], relation, keys)
     return len(keys)
+
