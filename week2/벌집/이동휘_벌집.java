28ef9335768a7a3626edc2a0d61bb9f1e5bf5e56
master <laksj113@gmail.com>
2023-08-04 13:14:31 +0900

    이동휘: 벌집(23.08.04)

--- week2/벌집/이동휘_벌집.java
+++ week2/벌집/이동휘_벌집.java
@@ -11,6 +11,9 @@ public class 이동휘_벌집 {
                 System.out.println(1);
                 return;
             }
+            // a0 = 1, a1 = 7, a2 = 19, a3 = 37 ...
+            // an = (6*n+6)*n/2 + 1 이고
+            // ai가 N보다 커지는 지점이 N이 가능해 지는 범위 이다
             if (N<=((6*i+6)*i/2)+1) {
                 System.out.println(i+1);
                 return;
