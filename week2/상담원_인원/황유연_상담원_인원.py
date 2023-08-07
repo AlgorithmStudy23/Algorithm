65330fcf3999271fae8da4fba2c76f7d8b0bdfec
YEONI <89332391+yoooyeon@users.noreply.github.com>
2023-08-07 22:14:18 +0900

    황유연: 상담원 인원 수정 (23.08.07)

--- week2/상담원_인원/황유연_상담원_인원.py
+++ week2/상담원_인원/황유연_상담원_인원.py
@@ -1,36 +1,46 @@
+from heapq import heappush, heappop
+
+
 def solution(k, n, reqs):  # 유형, 인원수, 요청
     answer = float('inf')
-    arr = [1 for _ in range(k + 1)]
-    arr[0] = 0
+    memo = [[0] * 21 for _ in range(k + 1)]
 
-    def calc_time(arr):
-        nonlocal answer
-        finish_time = []
-        latency = 0
-        for x in arr:
-            finish_time.append([0] * x)
-        for a, b, c in reqs:
-            if latency >= answer:
-                return float('inf')
-            ctime = finish_time[c]  # c 유형의 시작 가능한 상담원 시간
-            ctime.sort()
-            if ctime[0] <= a:
-                ctime[0] = a + b  # 원하는 시간부터 시작해서 종료, 종료 시간 저장
-            else:
-                latency += (ctime[0] - a)
-                ctime[0] = ctime[0] + b
-        return latency
+    def calc_time(arr):  # memo에서 보고 시간 가져오기
+        time = 0
+        for i in range(k):  # 0, 1, 2 유형
+            time += memo[i][arr[i] - 1]
+        return time
 
-    def dfs(n, arr):
+    def dfs(total, div, path):  # 상담사 몇명 배치 하는지 조합
         nonlocal answer
-        if sum(arr) == n:
-            # print(arr)
+        if div == 1:
+            arr = path + [total]
             answer = min(answer, calc_time(arr.copy()))
             return
-        for i in range(1, k + 1):
-            arr[i] += 1
-            dfs(n, arr)
-            arr[i] -= 1
+        for i in range(1, total - div + 2):
+            dfs(total - i, div - 1, path + [i])
 
-    dfs(n, arr)
+    def get_waiting_time(n, table):  # n명의 상담사, 시간표가 주어졌을 때 소요 시간
+        q = [0 for _ in range(n)]
+        waiting_time = 0
+        for start, dur in table:
+            available = heappop(q)
+            if available <= start:
+                heappush(q, start + dur)
+            else:
+                heappush(q, available + dur)
+                waiting_time += available - start
+        return waiting_time
+
+    table = [[] for _ in range(k)]
+    for a, b, c in reqs:
+        table[c - 1].append([a, b])
+
+    MAX = n - k + 1
+    memo = [[0 for _ in range(MAX)] for _ in range(k)]  # 행: 상담 유형, 열: 상담사 최대 배정 수
+    for i in range(k):
+        for j in range(1, MAX + 1):
+            memo[i][j - 1] = get_waiting_time(j, table[i])
+    dfs(n, k, [])
     return answer
+
