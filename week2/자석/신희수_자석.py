f8f0f2213c52f11f5ebe85dfc0bf4854b1d2936e
Heeesuu <gmltn0508@icloud.com>
2023-08-07 10:40:39 +0900

    신희수:알고스팟(2023.08.07)

--- week2/자석/신희수_자석.py
+++ week2/자석/신희수_자석.py
@@ -13,7 +13,6 @@ print(result)
 # result 값들을 저장하는 리스트
 results = []
 
-# 나머지 값들과 비교하여 최댓값 찾기
 for i in range(1, n+1):
     value = num2[i] - min_value - (k * abs(i - min_value_index))
     results.append(value)
