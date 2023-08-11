543153faaad767d16a8b7bb9e7c7ee117633d2a7
master <laksj113@gmail.com>
2023-08-11 14:23:39 +0900

    이동휘: 뉴스_전하기(23.08.11)

--- week3/뉴스_전하기/이동휘_뉴스_전하기.java
+++ week3/뉴스_전하기/이동휘_뉴스_전하기.java
@@ -1,18 +1,18 @@
-package week3.뉴스_정하기;
+package week3.뉴스_전하기;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
-public class 이동휘_뉴스_정하기 {
+public class 이동휘_뉴스_전하기 {
 
     static ArrayList<LinkedList<Integer>> graph;
-    static int n;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         graph = new ArrayList<>(n);
         for (int i = 0; i < n; i++) {
             graph.add(new LinkedList<>());
@@ -23,30 +23,28 @@ public class 이동휘_뉴스_정하기 {
             int cur = Integer.parseInt(st.nextToken());
             graph.get(cur).add(i);
         }
-        int[] arr = dfs(0);
-        for (int i = 50; i >= 1; i--) {
-            if (arr[i] > 0) {
-                System.out.println(i);
-                return;
-            }
-        }
+
+        System.out.println(dfs(0) - 1);
     }
-    static int[] dfs(int cur) {
+    static int dfs(int cur) {
         LinkedList<Integer> list = graph.get(cur);
         if (list.size() == 0) {
-            return new int[]{0, 1};
+            return 1;
         }
-        int[] num = new int[51];
+
+        int max = list.size();
+        int[] arr = new int[max];
+        int idx = 0;
         for (int i : list) {
-            int idx = 0;
-            for (int j : dfs(i)) {
-                num[idx++] += j;
-            }
+            arr[idx++] = dfs(i);
         }
-        for (int i = 1; i < 50; i++) {
-            num[i+1] += num[i] / 2;
-            num[i] %= 2;
+        Arrays.sort(arr);
+        int i = max-1;
+        int time = 0;
+        for (; i >= 0; i--) {
+            max = Math.max(max, time++ + arr[i]);
         }
-        return num;
+
+        return max + 1;
     }
 }
