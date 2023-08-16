31f83d4c40607925d9efb617b066e025467722be
master <laksj113@gmail.com>
2023-08-16 10:28:32 +0900

    이동휘: 타임머신(23.08.14)

---
+++ week4/상범_빌딩/이동휘_상범_빌딩.java
@@ -0,0 +1,9 @@
+package week4.상범_빌딩;
+import java.io.*;
+
+public class 이동휘_상범_빌딩 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+}
