e7d2cc961b5fc0727301d0f22face98703dba288
master <laksj113@gmail.com>
2023-08-23 12:07:25 +0900

    이동휘: 플로이드(23.08.23)

--- week4/플로이드/이동휘_플로이드.java
+++ week4/플로이드/이동휘_플로이드.java
@@ -1,9 +1,43 @@
 package week4.플로이드;
+
 import java.io.*;
+import java.util.*;
 
 public class 이동휘_플로이드 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int INF = 1<<28;
+        long[][] dp = new long[n][n];
+        for(int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], INF);
+        }
+        StringTokenizer st;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken())-1;
+            int b = Integer.parseInt(st.nextToken())-1;
+            int c = Integer.parseInt(st.nextToken());
+            dp[a][b] = Math.min(dp[a][b], c);
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i == j) continue;
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j]);
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (long j : dp[i]) {
+                bw.write(((j == INF) ? 0 : j) + " ");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
     }
 }
